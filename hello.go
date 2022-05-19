ddb39d0a8c818604dfd58b1b1c0fc30c5ca86341
Chinmaya Kaundanya <38719024+chinya07@users.noreply.github.com>
2022-05-19 19:18:14 +0530

    includes basic print, variable declaration, arrays, dictionaries

---
+++ hello.go
@@ -0,0 +1,57 @@
+// package main
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	fmt.Println("Hello world")
+// }
+// ############################################################
+// package main
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	x:= 7
+// 	y:= 4
+// 	if x<y {
+// 		fmt.Println("x is less than y")
+// 	} else {
+// 		fmt.Println("y is less than x")
+// 	}
+
+// }
+// ############################################################
+// package main
+// import (
+// 	"fmt"
+// )
+
+// func main() {
+// 	a:= []int {2,5,3,6,7}
+// 	fmt.Println(a)
+
+
+// }
+
+// import (
+// 	"fmt"
+// )
+// ############################################################
+package main
+import (
+	"fmt"
+)
+
+func main() {
+	dict1 := make(map[string]int)
+
+	dict1["Maths"]=99
+	dict1["Physics"]=96
+	dict1["Chemistry"]=92
+	fmt.Println(dict1)
+
+
+}
+
